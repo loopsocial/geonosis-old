bf5831b852537ff3bae06cad222649f44f96d8e3
guwenshuai <ma_lijia@cdv.com>
2021-03-24 23:20:06 +0800

    fix: 接口host使用 studio.sandbox

--- src/api/apiHost.js
+++ src/api/apiHost.js
@@ -1,9 +1,10 @@
 // 生成环境，不同API前缀会使用不同的Host，与vue.config.js中proxy代理一致
 export default {
-  fw:
-    process.env.NODE_ENV === "production"
-      ? "https://api.fw.tv"
-      : "https://studio.sandbox.fireworktv.com",
+  // fw:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://api.fw.tv"
+  //     : "https://studio.sandbox.fireworktv.com",
+  fw: "https://studio.sandbox.fireworktv.com",
   fwServe: "http://meishe.fireworktv.net/",
   ms: "https://testeapi.meishesdk.com:8443"
 };
